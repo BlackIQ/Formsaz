93fb270ec1d5ac527ae0519520708c2580ac124e
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-21 15:02:18 +0430

    Add name

--- api/app/models/form_model.js
+++ api/app/models/form_model.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 
 const formModel = new Schema(
     {
+        view: {
+            type: String,
+            required: true,
+        },
         name: {
             type: String,
             required: true,
