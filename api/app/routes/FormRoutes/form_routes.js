bf92695604d4f4399a5f443c7434932e071422c8
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-21 15:02:02 +0430

    Add get all forms

--- api/app/routes/FormRoutes/form_routes.js
+++ api/app/routes/FormRoutes/form_routes.js
@@ -5,6 +5,7 @@ const controllers = require("../../controllers/FormControllers/form_controllers"
 const router = express.Router();
 
 router.post('/create', controllers.createForm);
+router.get('/all', controllers.allForms);
 router.get('/get/:form_id', controllers.showForm);
 
 module.exports = router;
