67daa3e303a084701314a3152be19a89c0fb22c6
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-21 13:12:31 +0430

    Create routes and using cors

--- api/app/app.js
+++ api/app/app.js
@@ -1,1 +1,18 @@
-const
+const express = require("express");
+const cors = require("cors");
+
+const FieldRoutes = require("./routes/FieldRoutes/field_routes");
+const FormRoutes = require("./routes/FormRoutes/form_routes");
+
+const app = express();
+
+app.set('json spaces', 2);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/api/field', FieldRoutes);
+app.use('/api/form', FormRoutes);
+
+module.exports = app;
