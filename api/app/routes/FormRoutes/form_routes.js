4abca22304f9a8961579e5b7d964317b88dc9a25
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-22 08:13:28 +0430

    Add delete form route

--- api/app/routes/FormRoutes/form_routes.js
+++ api/app/routes/FormRoutes/form_routes.js
@@ -4,8 +4,9 @@ const controllers = require("../../controllers/FormControllers/form_controllers"
 
 const router = express.Router();
 
-router.post('/create', controllers.createForm);
 router.get('/all', controllers.allForms);
 router.get('/get/:form_id', controllers.showForm);
+router.post('/create', controllers.createForm);
+router.delete('/delete/:form_id', controllers.deleteForm);
 
 module.exports = router;
