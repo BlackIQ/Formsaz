21b143191f0c7d5b738c6c1c8f4c4b54016c4da2
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-23 15:47:26 +0430

    Add test api route

--- api/app/app.js
+++ api/app/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const FieldRoutes = require("./routes/FieldRoutes/field_routes");
 const FormRoutes = require("./routes/FormRoutes/form_routes");
+const TestRoutes = require("./routes/TestRoutes/test_routes");
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(cors());
 
 app.use('/api/field', FieldRoutes);
 app.use('/api/form', FormRoutes);
+app.use('/api/test', TestRoutes);
 
 module.exports = app;
