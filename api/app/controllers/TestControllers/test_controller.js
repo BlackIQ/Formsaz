37e239e2a86e5b39fde3838ac860424cc7eeb2ea
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-24 22:38:58 +0430

    Working on it

--- api/app/controllers/TestControllers/test_controller.js
+++ api/app/controllers/TestControllers/test_controller.js
@@ -1,4 +1,48 @@
-const Test = require("../../models/test_model");
+const mongoose = require("mongoose");
+
+const Form = require("../../models/form_model");
+const Field = require("../../models/field_model");
+
+const {data_type} = require("../../hooks/model_creator");
+
+const test = (req, res) => {
+    const {form_id} = req.body;
+
+    Form.findById(form_id)
+        .then((form_result) => {
+            Field.find({form: form_result._id})
+                .then((fields_result) => {
+                    const Schema = mongoose.Schema;
+                    fields_result.map(field => {
+                    //     let newModel;
+                    //    newModel =  new Schema(
+                    //        {
+                    //             type: "string",
+                    //             default: field.default,
+                    //             required: field.required,
+                    //             unique: field.unique,
+                    //         }
+                    //         ,
+                    //         {
+                    //             timestamps: true,
+                    //         },
+                    //     );
+                    //     mongoose.model(form_result.name, newModel);
+                        console.log("field",field)
+
+})
+             
+
+
+                 
+                    res.send({message: "success"});
+
+                    // res.send(fields_result);
+                })
+                .catch((error) => res.status(500).send(error));
+        })
+        .catch((error) => res.status(500).send(error));
+}
 
 const insert = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -43,4 +87,5 @@ const read = (req, res) => {
 module.exports = {
     insert,
     read,
+    test,
 }
