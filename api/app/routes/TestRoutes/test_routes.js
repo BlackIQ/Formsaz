b7132b6807edd46e48edd1b90ee90a30035878c7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-24 22:38:43 +0430

    New test route

--- api/app/routes/TestRoutes/test_routes.js
+++ api/app/routes/TestRoutes/test_routes.js
@@ -4,6 +4,7 @@ const controllers = require("../../controllers/TestControllers/test_controller")
 
 const router = express.Router();
 
+router.post('/test', controllers.test);
 router.get('/read', controllers.read);
 router.post('/insert', controllers.read);
 
