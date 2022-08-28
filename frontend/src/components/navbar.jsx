c7f11e6e8b6c7c567538515fe04921e51a80f180
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-28 15:18:17 +0430

    Fix field dialog and adding

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 import {
     AppBar,
@@ -14,30 +14,86 @@ import {
     DialogTitle,
     DialogContent,
     DialogActions,
+    FormControlLabel,
+    Checkbox,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select,
 } from "@mui/material";
 
 import Axios from "axios";
 
 const baseUrl = process.env.REACT_APP_BACKEND_URL;
 
+const datatypes = [
+    {
+        title: "String",
+        value: "string",
+    },
+    {
+        title: "Number",
+        value: "number",
+    },
+    {
+        title: "Boolean",
+        value: "boolean",
+    }
+];
+
 const Navbar = () => {
     const history = useHistory();
+    const location = useLocation();
+
+    const splitedLocation = location.pathname.split('/');
+    const form_id = splitedLocation.at(-1);
 
     const [addFormDialogOpen, setAddFormDialogOpen] = useState(false);
+    const [fieldDialog, setFieldDialog] = useState(false);
 
     const [viewName, setViewName] = useState('');
+    const [defValue, setDefValue] = useState('');
+    const [datatype, setDatatype] = useState('string');
+    const [isRequired, setIsRequired] = useState(false);
+    const [isUnique, setIsUnique] = useState(false);
+
+    const addField = () => {
+        const sendData = {
+            view: viewName.charAt(0).toLocaleUpperCase() + viewName.slice(1),
+            name: viewName.toLowerCase() + '_item',
+            type: datatype,
+            required: isRequired,
+            unique: isUnique,
+            default: defValue,
+            form: form_id,
+        };
+
+        Axios.post(`${baseUrl}/api/field/create`, sendData)
+            .then((result) => {
+                setFormViewName('');
+                setDefValue('');
+                setDatatype('string');
+                setIsRequired(false);
+                setIsUnique(false);
+
+                setFieldDialog(false);
+            })
+            .catch((error) => console.log(error));
+    }
+
+    const [formViewName, setFormViewName] = useState('');
 
     const createForm = () => {
         const sendData = {
-            view: viewName.charAt(0).toLocaleUpperCase() + viewName.slice(1),
-            name: viewName.toLowerCase() + '_db',
+            view: formViewName.charAt(0).toLocaleUpperCase() + formViewName.slice(1),
+            name: formViewName.toLowerCase() + '_db',
         };
 
         Axios.post(`${baseUrl}/api/form/create`, sendData)
             .then((result) => {
                 const form_id = result.data.form._id;
 
-                setViewName('');
+                setFormViewName('');
                 setAddFormDialogOpen(false);
 
                 history.push(`/form/${form_id}`);
@@ -69,6 +125,17 @@ const Navbar = () => {
                         >
                             Create a new form
                         </Button>
+                        {
+                            splitedLocation.at(-2) === "form"
+                            &&
+                            <Button
+                                variant="text"
+                                color="inherit"
+                                onClick={() => setFieldDialog(true)}
+                            >
+                                Add a new field
+                            </Button>
+                        }
                     </Toolbar>
                 </Container>
             </AppBar>
@@ -89,8 +156,8 @@ const Navbar = () => {
                         color="primary"
                         label="Name"
                         placeholder="Enter the form name"
-                        value={viewName}
-                        onChange={(e) => setViewName(e.target.value)}
+                        value={formViewName}
+                        onChange={(e) => setFormViewName(e.target.value)}
                         margin="dense"
                         fullWidth
                     />
@@ -103,6 +170,88 @@ const Navbar = () => {
                         disableElevation
                     >
                         Create form
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog
+                open={fieldDialog}
+                onClose={() => setFieldDialog(false)}
+                maxWidth="xs"
+                fullWidth
+            >
+                <DialogTitle>
+                    <Typography variant="h6" color="primary.main">Add new field</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <br/>
+                    <FormControl fullWidth>
+                        <InputLabel>Data Type</InputLabel>
+                        <Select
+                            variant="standard"
+                            label="Data type"
+                            value={datatype}
+                            onChange={(e) => setDatatype(e.target.value)}
+                        >
+                            {
+                                datatypes.map((datat) => (
+                                    <MenuItem
+                                        key={datat}
+                                        value={datat.value}
+                                    >
+                                        {datat.title}
+                                    </MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
+                    <TextField
+                        variant="standard"
+                        color="primary"
+                        label="Field name"
+                        placeholder="Enter the name that is going to call it"
+                        value={viewName}
+                        onChange={(e) => setViewName(e.target.value)}
+                        margin="dense"
+                        fullWidth
+                    />
+                    <TextField
+                        variant="standard"
+                        color="primary"
+                        label="Default value"
+                        placeholder="Enter the default value of the item"
+                        value={defValue}
+                        onChange={(e) => setDefValue(e.target.value)}
+                        margin="dense"
+                        fullWidth
+                    />
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                value={isRequired}
+                                onChange={() => setIsRequired(!isRequired)}
+                            />
+                        }
+                        label="Required"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                value={isUnique}
+                                onChange={() => setIsUnique(!isUnique)}
+                            />
+                        }
+                        label="Unique"
+                    />
+                </DialogContent>
+                <DialogActions sx={{p: 3}}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => addField()}
+                        disableElevation
+                    >
+                        Create field
                     </Button>
                 </DialogActions>
             </Dialog>
