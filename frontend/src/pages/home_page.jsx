2a82afc47a566de59eea43dd3e0de5bfc029fc22
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-28 14:53:48 +0430

    Added action buttons

--- frontend/src/pages/home_page.jsx
+++ frontend/src/pages/home_page.jsx
@@ -4,13 +4,18 @@ import {useHistory} from "react-router-dom";
 import {
     Box,
     List,
-    ListItemButton,
+    ListItem,
     ListItemText,
-    ListItemIcon, CircularProgress,
+    ListItemIcon,
+    CircularProgress,
+    IconButton,
 } from "@mui/material";
 
 import {
     Folder,
+    FileOpen,
+    Delete,
+    Edit,
 } from "@mui/icons-material";
 
 import Axios from "axios";
@@ -21,6 +26,12 @@ const HomePage = () => {
     const history = useHistory();
 
     const [forms, setForms] = useState('');
+
+    const deleteForm = (form_id) => {
+        Axios.delete(`${baseUrl}/api/form/delete/${form_id}`)
+            .then((result) => history.push("/"))
+            .catch((error) => console.log(error));
+    }
 
     useEffect(() => {
         Axios.get(`${baseUrl}/api/form/all`)
@@ -36,15 +47,30 @@ const HomePage = () => {
                     <List>
                         {
                             forms.map((form) => (
-                                <ListItemButton
-                                    Key={form._id}
-                                    onClick={() => history.push(`/form/${form._id}`)}
+                                <ListItem
+                                    key={form}
+                                    secondaryAction={
+                                        <Box>
+                                            <IconButton onClick={() => deleteForm(form._id)}>
+                                                <Delete color="error" />
+                                            </IconButton>
+                                            &nbsp;
+                                            <IconButton onClick={() => history.push(`/show/${form._id}`)}>
+                                                <FileOpen color="info" />
+                                            </IconButton>
+                                            &nbsp;
+                                            <IconButton onClick={() => history.push(`/form/${form._id}`)}>
+                                                <Edit color="warning" />
+                                            </IconButton>
+                                        </Box>
+                                    }
+                                    
                                 >
                                     <ListItemIcon>
-                                        <Folder color="primary"/>
+                                        <Folder color="primary" fontSize="large" />
                                     </ListItemIcon>
-                                    <ListItemText primary={form.view}/>
-                                </ListItemButton>
+                                    <ListItemText primary={form.view} />
+                                </ListItem>
                             ))
                         }
                     </List>
