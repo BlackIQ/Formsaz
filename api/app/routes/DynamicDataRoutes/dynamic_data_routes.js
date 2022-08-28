e5bfdcea03bfec18cad21d50ea9b2390ed85138a
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-28 15:50:20 +0430

    Refactor routes

---
+++ api/app/routes/DynamicDataRoutes/dynamic_data_routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const controllers = require("../../controllers/DynamicDataControllers/dynamic_data_controllers");
+
+const router = express.Router();
+
+router.post('/test', controllers.test);
+router.get('/read/:form_id', controllers.read);
+router.post('/insert', controllers.insert);
+
+module.exports = router;
